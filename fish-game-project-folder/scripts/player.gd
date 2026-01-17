c1b6ab4e7a00cfc5fc6188ab4a67418dfa8b5024
Andreas Buzan <aabuzan@unc.edu>
2026-01-17 15:06:27 -0500

    made collectable green

--- fish-game-project-folder/scripts/player.gd
+++ fish-game-project-folder/scripts/player.gd
@@ -10,6 +10,9 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
+func _ready() -> void:
+	print('total collectables: ', self.collectables)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not IS_IN_WATER:
@@ -23,13 +26,19 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "foward", "back")
 	
+	# If we're in water, use Space and Shift to move up and down.
+	# Else, cant move up and down with controlls. Velocity is controlled by jump
 	var up_down_dir
 	if IS_IN_WATER:
 		up_down_dir = Input.get_axis("down", "up")
 	else:
 		up_down_dir = 0
 	
+	# Sets the direction of movement based on input_dir and up_down_dir
 	var direction := (transform.basis * Vector3(input_dir.x, up_down_dir, input_dir.y)).normalized()
+	
+	# Set speed in the direction
+	# If in water, y direction is considered as well
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -43,12 +52,13 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
+	# If the raycast sees an object, the player presses E, and the object is a collectable
 	if (
 		ray_cast_3d.is_colliding() 
 		and Input.is_action_just_pressed("interact") 
 		and (ray_cast_3d.get_collider().get_parent().is_in_group('collectable'))
 	):
+		# Increase the total number of collectables and delete the interacted object
 		self.collectables += 1
-		print('collect')
 		print('total collectables: ', self.collectables)
 		ray_cast_3d.get_collider().get_parent().queue_free()
