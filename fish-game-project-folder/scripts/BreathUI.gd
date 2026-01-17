a9cef57fd5dc87f2f874d4e1b30a0afedfe1e476
Benjamin <benjaminrimon@gmail.com>
2026-01-17 16:59:43 -0500

    Added breath system to ben scene
    ..

---
+++ fish-game-project-folder/scripts/BreathUI.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var label: Label = $Panel/Label
+@onready var player := get_node("/root/Ben/Player")
+
+func _ready() -> void:
+	player.breath_updated.connect(set_breath)
+	set_breath(player.breath, player.breath_max)
+
+func set_breath(current: float, max_value: float) -> void:
+	var secs := int(ceil(clamp(current, 0.0, max_value)))
+	label.text = "Breath: %d:%02d" % [secs / 60, secs % 60]
+	visible = secs < int(max_value) 
