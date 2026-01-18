d731140b026e71d8640dae384125dd7fca702057
Andreas Buzan <aabuzan@unc.edu>
2026-01-17 19:45:34 -0500

    start of holdable items

---
+++ fish-game-project-folder/scripts/test_holdable.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var IS_HELD: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_interact(player: CharacterBody3D) -> void:
+	pass
