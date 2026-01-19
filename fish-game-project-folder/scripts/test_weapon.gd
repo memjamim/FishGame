374e2048f33747cd587ee6ddc9b61ca28a030e6d
Andreas Buzan <aabuzan@unc.edu>
2026-01-19 18:02:10 -0500

    hover highlight shader
    added a lil shader to tell when player can interact with an object, extended their reach, and a crosshair

--- fish-game-project-folder/scripts/test_weapon.gd
+++ fish-game-project-folder/scripts/test_weapon.gd
@@ -1,7 +1,9 @@
 extends RigidBody3D
 
 @onready var hitbox: Area3D = $Hitbox
+@onready var shader = $MeshInstance3D.mesh.material.next_pass
 
+var targeted = false : set = _set_targeted
 
 signal enemy_hit(body_hit)
 
@@ -14,6 +16,12 @@ signal enemy_hit(body_hit)
 #func _process(delta: float) -> void:
 	#pass
 
+func _set_targeted(val):
+	targeted = val
+	if targeted:
+		shader.set_shader_parameter('strength', 0.5)
+	else:
+		shader.set_shader_parameter('strength', 0.0)
 
 
 func _on_hitbox_body_entered(body: Node3D) -> void:
